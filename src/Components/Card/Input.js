04b0bab7ef181b030cb32107e6c4ccc54f931b09
Niour <38693643+Niour@users.noreply.github.com>
2020-09-18 15:42:14 +0300

    commit update card component

--- src/Components/Card/Input.js
+++ src/Components/Card/Input.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React from "react";
 
-export const Input = (
-    props
-) => { 
-    return (
-    <label> 
-        {props.label}
-    <input type={props.type} value={props.value} onChange={props.onChange} />
+export const Input = (props) => {
+  return (
+    <label>
+      {props.label}
+      <input type={props.type} value={props.value} onChange={props.onChange} />
     </label>
-    )
+  );
 };
 
+// width: 100%;
+// padding: 12px 20px;
+// margin: 8px 0;
+// display: inline-block;
+// border: 1px solid #ccc;
+// border-radius: 4px;
+// box-sizing: border-box;
